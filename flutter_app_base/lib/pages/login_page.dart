d93e4d5a865affb15d3ba5b9d125cfe784404d77
Milene <milene.lopes@unimedflorianopolis.com.br>
2021-03-03 08:22:45 -0300

    recuperar dados digitados no TextFormField

--- flutter_app_base/lib/pages/login_page.dart
+++ flutter_app_base/lib/pages/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,36 +20,23 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          _text("Login", "Digite seu login"),
+          _text("Login", "Digite seu login", controller: _tLogin),
           SizedBox(
             height: 10,
           ),
-          _text("Senha", "Digite sua senha", password: true),
+          _text("Senha", "Digite sua senha", password: true, controller: _tSenha),
           SizedBox(
             height: 20,
           ),
-          _button("Login"),
+          _button("Login", _onClickLogin),
         ],
       ),
     );
   }
 
-  _button(String text) {
-    return Container(
-      height: 46,
-      child: RaisedButton(
-        color: Colors.blue,
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.white, fontSize: 22),
-        ),
-        onPressed: () {},
-      ),
-    );
-  }
-
-  _text(String label, String hint, {bool password = false}) {
+  _text(String label, String hint, {bool password = false, TextEditingController controller}) {
     return TextFormField(
+      controller: controller,
       obscureText: password,
       style: TextStyle(
         fontSize: 25,
@@ -63,5 +53,26 @@ class LoginPage extends StatelessWidget {
             fontSize: 16,
           )),
     );
+  }
+
+  _button(String text, Function onPressed) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 22),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+
+  _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
   }
 }
