5c809d494de26da0aad405b58894a95973f27acd
Milene <milene.lopes@unimedflorianopolis.com.br>
2021-03-02 22:39:43 -0300

    Criado loginPage

---
+++ flutter_app_base/lib/pages/login_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          Text("Login"),
+          TextFormField(),
+          SizedBox(height: 10,),
+          Text("Senha"),
+          TextFormField(
+            obscureText: true,
+          ),
+          SizedBox(height: 20,),
+          Container(
+            height: 46,
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                "Login",
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
